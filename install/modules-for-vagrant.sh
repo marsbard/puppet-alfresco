4868d8334c631058952482c9e24b33c395f1bc76
Martin Cosgrave <martin@ciderpunk.com>
2014-12-31 18:52:57 +0100

    Revert "added epel"
    This reverts commit 8f0c2503075fcc9e9e49a9c5070fe07f5af724ab.

--- install/modules-for-vagrant.sh
+++ install/modules-for-vagrant.sh
@@ -2,7 +2,7 @@
 
 
 mkdir -p modules
-for mod in puppetlabs-mysql puppetlabs-stdlib stahnma/epel
+for mod in puppetlabs-mysql puppetlabs-stdlib
 do
 	puppet module install --force $mod --target-dir=modules
 done
