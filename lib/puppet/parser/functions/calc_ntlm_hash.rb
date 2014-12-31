88932cdc352b6eb6fbc908d56357d0981a880417
Martin Cosgrave <martin@ciderpunk.com>
2014-12-31 18:57:54 +0100

    removed comments

--- lib/puppet/parser/functions/calc_ntlm_hash.rb
+++ lib/puppet/parser/functions/calc_ntlm_hash.rb
@@ -9,9 +9,9 @@ module Puppet::Parser::Functions
 		#   PASSWD_HASH=`iconv -f ASCII -t UTF-16LE <(printf "${PASSWD}") | openssl dgst -md4 | cut -f2 -d\ `
 
 		command = Shellwords.escape('iconv -f ASCII -t UTF-16LE <(printf "' << admin_pass << '") | openssl dgst -md4 | cut -f2 -d\ ')
-		#warn("command=" << command)
+
 		hash = `bash -c #{command}`
-		#warn("hash=" << hash)
-		return hash
+		
+    return hash
 	end
 end
