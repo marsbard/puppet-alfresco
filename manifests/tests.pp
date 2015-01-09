2b6c6bdd6cdb425afb7259024acfda918a2f734c
Martin Cosgrave <martin@ciderpunk.com>
2015-01-09 14:14:46 +0100

    try to use Zaizi basic tests

--- manifests/tests.pp
+++ manifests/tests.pp
@@ -1,14 +1,61 @@
 class alfresco::tests inherits alfresco {
 
-  package { 'phantomjs':
+  package { 'mvn':
     ensure => latest,
   }
 
-  $testdir = '/opt/alfresco/tests'
-
-  file { $testdir:
+  file { "${alfresco_base_dir}/tests":
     ensure => directory,
   }
+
+  exec { "clone-zaizi-tests":
+    comand => 'git clone https://github.com/zaizi/alfresco-test-scripts.git',
+    cwd => "${alfresco_base_dir}/tests",
+    creates => "${alfresco_base_dir}/tests/alfresco-test-scripts/README.md",
+    path => '/usr/bin',
+  }
+
+  $testbase = "${alfresco_base_dir}/tests/alfresco-test-scripts/ABFT_4_2"
+  $confloc = ${testbase}/src/test/resources"
+
+  file { "${confloc}/TestProperties.xml":
+    content => template('alfresco/ZaiziTestProperties.xml.erb'),
+    ensure => present,
+    require => Exec['clone-zaizi-tests'],
+  }
+
+  file { "${confloc}/TestValues.xml":
+    content => template('alfresco/ZaiziTestValues.xml.erb'),
+    ensure => present,
+    require => Exec['clone-zaizi-tests'],
+  }
+
+  file { "${testbase}/AbftsUploadTest.txt":
+    source => 'puppet:///modules/alfresco/ZaiziAbftsUploadTest.txt',
+    ensure => present,
+    require => Exec['clone-zaizi-tests'],
+  }
+
+  exec { "run-tests":
+    command => "mvn test",
+    cwd => $testbase,
+    path => '/usr/bin',
+    require => [
+      File["${confloc}/TestProperties.xml"],
+      File["${confloc}/TestValues.xml"],
+      File["${testbase}/AbftsUploadTest.txt"],
+    ],
+  }
+
+#  package { 'phantomjs':
+#    ensure => latest,
+#  }
+#
+#  $testdir = '/opt/alfresco/tests'
+#
+#  file { $testdir:
+#    ensure => directory,
+#  }
 
 #  $tests = [
 #    "login.js",
@@ -28,17 +75,17 @@ class alfresco::tests inherits alfresco {
 #  }
 
 
-  file { "${testdir}/login.js":
-    ensure => present,
-    content => template('alfresco/tests/login.js.erb'),
-    require => File[$testdir],
-  }
-
-  file { "${testdir}/test.sh":
-    ensure => present,
-    content => template('alfresco/test.sh.erb'),
-    require => File[$testdir],
-    mode => '0755',
-  }
+#  file { "${testdir}/login.js":
+#    ensure => present,
+#    content => template('alfresco/tests/login.js.erb'),
+#    require => File[$testdir],
+#  }
+#
+#  file { "${testdir}/test.sh":
+#    ensure => present,
+#    content => template('alfresco/test.sh.erb'),
+#    require => File[$testdir],
+#    mode => '0755',
+#  }
 
 }
