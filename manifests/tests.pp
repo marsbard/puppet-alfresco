392ccbf21be5e82cd8f05fc9e81eec914fe53f5e
Martin Cosgrave <martin@ciderpunk.com>
2015-01-17 13:19:39 +0100

    moving to digcat tests

--- manifests/tests.pp
+++ manifests/tests.pp
@@ -1,91 +1,61 @@
 class alfresco::tests inherits alfresco {
 
-  package { [ 'maven', 'firefox', 'xvfb', 'postfix' ]:
+  $packages = [ 
+    'python',
+    'xvfb', 'x11-xkb-utils',
+    'xfonts-100dpi', 'xfonts-75dpi', 
+    'xfonts-scalable', 
+    'xfonts-cyrillic', 
+    'x11-apps',
+    'python-yaml',
+    'firefox',
+  ]
+
+  package {  $packages :
     ensure => latest,
+  }
+
+  exec { "install cmislib":
+    command => "easy_install cmislib",
+    path => "/usr/bin",
+# TODO - find out what this creates and put it here:
+    # creates => "/usr/lib/cmislib???",
+    require => Package['python'],
+  }
+
+  python::pip { 'configure':
+    owner => 'root',
+    pkgname => 'configure',
+    require => Package['python'],
+  }
+
+  python::pip { 'configuration':
+    owner => 'root',
+    pkgname => 'configuration',
+    require => Package['python'],
+  }
+
+  python::pip { 'selenium':
+    owner => 'root',
+    pkgname => 'selenium',
+    require => Package['python'],
+  }
+
+  exec { "clone-digcat-tests":
+    command => "git clone https://github.com/digcat/alfresco-tests.git",
+    path => "/usr/bin",
+    cwd => '${alfresco_base_dir}/tests',
+    require => File['${alfresco_base_dir}/tests'],
   }
 
   file { "${alfresco_base_dir}/tests":
     ensure => directory,
   }
 
-  exec { "clone-zaizi-tests":
-    command => 'git clone https://github.com/marsbard/alfresco-test-scripts.git',
-    cwd => "${alfresco_base_dir}/tests",
-    creates => "${alfresco_base_dir}/tests/alfresco-test-scripts/README.md",
-    path => '/usr/bin',
-  }
-
-  $testbase = "${alfresco_base_dir}/tests/alfresco-test-scripts/ABFT_4_2"
-  $confloc = "${testbase}/src/test/resources"
-
-  file { "${confloc}/TestProperties.xml":
-    content => template('alfresco/ZaiziTestProperties.xml.erb'),
+  file { "${alfresco_base_dir}/tests/alfresco-tests/config.yml":
+    content => template('alfresco/tests-config.yml.erb'),
     ensure => present,
-    require => Exec['clone-zaizi-tests'],
+    require => Exec['clone-digcat-tests'],
   }
-
-  file { "${confloc}/TestValues.xml":
-    content => template('alfresco/ZaiziTestValues.xml.erb'),
-    ensure => present,
-    require => Exec['clone-zaizi-tests'],
-  }
-
-  file { "${testbase}/AbftsUploadTest.txt":
-    source => 'puppet:///modules/alfresco/ZaiziAbftsUploadTest.txt',
-    ensure => present,
-    require => Exec['clone-zaizi-tests'],
-  }
-
-#  exec { "run-tests":
-#    command => "mvn test",
-#    cwd => $testbase,
-#    path => '/usr/bin',
-#    require => [
-#      File["${confloc}/TestProperties.xml"],
-#      File["${confloc}/TestValues.xml"],
-#      File["${testbase}/AbftsUploadTest.txt"],
-#    ],
-#  }
-
-#  package { 'phantomjs':
-#    ensure => latest,
-#  }
-#
-#  $testdir = '/opt/alfresco/tests'
-#
-#  file { $testdir:
-#    ensure => directory,
-#  }
-
-#  $tests = [
-#    "login.js",
-#  ]
-#
-#  file { $tests: 
-#    content => template("alfresco/tests/${name}.erb"),
-#    path => "${testdir}/${name}",
-#    ensure => present,
-#    require => File[$testdir],
-#  }
-#
-#  exec { $tests:
-#    require => File[$title],
-#    command => "phantomjs ${testdir}/${title}",
-#    path => '/bin:/usr/bin',
-#  }
-
-
-#  file { "${testdir}/login.js":
-#    ensure => present,
-#    content => template('alfresco/tests/login.js.erb'),
-#    require => File[$testdir],
-#  }
-#
-#  file { "${testdir}/test.sh":
-#    ensure => present,
-#    content => template('alfresco/test.sh.erb'),
-#    require => File[$testdir],
-#    mode => '0755',
-#  }
 
 }
