77f0c6bbee1a0b5401905408a0714f0cdc9f2d27
Martin Cosgrave <martin@ciderpunk.com>
2015-01-19 19:49:43 +0100

    add a space before command containng password so it doesn't go in any history

--- lib/puppet/parser/functions/calc_ntlm_hash.rb
+++ lib/puppet/parser/functions/calc_ntlm_hash.rb
@@ -8,7 +8,7 @@ module Puppet::Parser::Functions
 		#   In shell it is this:
 		#   PASSWD_HASH=`iconv -f ASCII -t UTF-16LE <(printf "${PASSWD}") | openssl dgst -md4 | cut -f2 -d\ `
 
-		command = Shellwords.escape('iconv -f ASCII -t UTF-16LE <(printf "' << admin_pass << '") | openssl dgst -md4 | cut -f2 -d\ ')
+		command = Shellwords.escape(' iconv -f ASCII -t UTF-16LE <(printf "' << admin_pass << '") | openssl dgst -md4 | cut -f2 -d\ ')
 
 		hash = `bash -c #{command}`
 		
