e58d11c9d24fac250ed054f04d81b408919d7ef2
Martin Cosgrave <martin@ciderpunk.com>
2015-01-22 17:42:33 +0100

    fixed hardcoded download path and tidied up

--- manifests/addons/jsconsole.pp
+++ manifests/addons/jsconsole.pp
@@ -4,47 +4,48 @@ class alfresco::addons::jsconsole inherits alfresco::addons {
 	$url_jsconsole = "https://share-extras.googlecode.com/files/${filename_jsconsole}"
 
 
-        exec { "retrieve-jsconsole":
-                creates => "${downloads_path}/${filename_jsconsole}",
-                command => "wget ${url_jsconsole} -O ${downloads_path}/${filename_jsconsole}",
-                path => "/usr/bin",
-                require => File["/opt/downloads/jsconsole"],
-        }
+  exec { "retrieve-jsconsole":
+    creates => "${downloads_path}/${filename_jsconsole}",
+    command => "wget ${url_jsconsole} -O ${downloads_path}/${filename_jsconsole}",
+    path => "/usr/bin",
+    require => File["${download_path}/jsconsole"],
+  }
 
-        exec { "unpack-jsconsole":
-                creates => "/opt/downloads/jsconsole/README.txt",
-                cwd => "/opt/downloads/jsconsole",
-                command => "unzip -o ${downloads_path}/${filename_jsconsole}",
-                require => [
-                        File["/opt/downloads/jsconsole"],
-                        Exec["retrieve-jsconsole"],
+  exec { "unpack-jsconsole":
+    creates => "${download_path}/jsconsole/README.txt",
+    cwd => "${download_path}/jsconsole",
+    command => "unzip -o ${downloads_path}/${filename_jsconsole}",
+    require => [
+      File["${download_path}/jsconsole"],
+      Exec["retrieve-jsconsole"],
 			Package["unzip"],
-                ],
-                path => "/usr/bin",
-        }
+    ],
+    path => "/usr/bin",
+  }
 
 
-        file { "/opt/downloads/jsconsole":
-                ensure => directory,
-                before => Exec["unpack-jsconsole"],
-        }
+  file { "${download_path}/jsconsole":
+    ensure => directory,
+    before => Exec["unpack-jsconsole"],
+  }
 
-        file { "${alfresco_base_dir}/amps/javascript-console-repo-0.5.1.amp":
-                source => "/opt/downloads/jsconsole/4.0.x/javascript-console-repo-0.5.1.amp",
-                ensure => present,
-                require => [
-                        Exec["unpack-jsconsole"],
-                ],
+  file { "${alfresco_base_dir}/amps/javascript-console-repo-0.5.1.amp":
+    source => "${download_path}/jsconsole/4.0.x/javascript-console-repo-0.5.1.amp",
+    ensure => present,
+    require => [
+      Exec["unpack-jsconsole"],
+    ],
 		notify => Exec["apply-addons"],
-        }
-        file { "${alfresco_base_dir}/amps_share/javascript-console-share-0.5.1.amp":
-                source => "/opt/downloads/jsconsole/4.0.x/javascript-console-share-0.5.1.amp",
-                ensure => present,
-                require => [
-                        Exec["unpack-jsconsole"],
-                ],
+  }
+
+  file { "${alfresco_base_dir}/amps_share/javascript-console-share-0.5.1.amp":
+    source => "${download_path}/jsconsole/4.0.x/javascript-console-share-0.5.1.amp",
+    ensure => present,
+    require => [
+      Exec["unpack-jsconsole"],
+    ],
 		notify => Exec["apply-addons"],
-        }
+  }
 
 
 }
