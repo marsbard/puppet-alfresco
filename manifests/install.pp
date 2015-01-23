b56a0547bb6c109a6fce04115d1733e29ceff4fe
Martin Cosgrave <martin@ciderpunk.com>
2015-01-23 08:38:20 +0100

    refactored mysql into own manifest

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,25 +4,7 @@ class alfresco::install inherits alfresco {
 
   class { 'alfresco::install::alfresco-ce': }
   class { 'alfresco::install::postfix': }
-
-
-
-	class { '::mysql::server':
-		#bind_address => '0.0.0.0',
-		root_password    => $db_root_password,
-	}
-
-	mysql::db { "$alfresco_db_name":
-		user     => "${alfresco_db_user}",
-		password => "${alfresco_db_pass}",
-		host     => "${alfresco_db_host}",
-		grant    => ['ALL'],
-	}
-
-
-	class { '::mysql::bindings':
-		java_enable => 1,
-	}
+  class { 'alfresco::install::mysql': }
 
 
 
@@ -235,30 +217,6 @@ class alfresco::install inherits alfresco {
 
 
 
-	exec { "retrieve-mysql-connector":
-		command => "wget ${urls::mysql_connector_url}",
-		cwd => "${download_path}",
-		path => "/usr/bin",
-		creates => "${download_path}/${urls::mysql_connector_file}",
-	}
-
-	exec { "unpack-mysql-connector":
-		command => "tar xzvf ${urls::mysql_connector_file}",
-		cwd => $download_path,
-		path => "/bin",
-		require => Exec["retrieve-mysql-connector"],
-		creates => "${download_path}/${urls::mysql_connector_name}",
-	}
-
-	exec { "copy-mysql-connector":
-		command => "cp ${download_path}/${urls::mysql_connector_name}/${urls::mysql_connector_name}-bin.jar  ${tomcat_home}/shared/lib/",
-		path => "/bin:/usr/bin",
-		require => [
-			Exec["unpack-mysql-connector"],
-			File["${tomcat_home}/shared/lib"],
-		],
-		creates => "${tomcat_home}/shared/lib/${urls::mysql_connector_name}-bin.jar",
-	}
 
 	
 
---
+++ manifests/install/mysql.pp
@@ -0,0 +1,47 @@
+class alfresco::install::mysql inherits alfresco {
+
+
+	class { '::mysql::server':
+		#bind_address => '0.0.0.0',
+		root_password    => $db_root_password,
+	}
+
+	mysql::db { "$alfresco_db_name":
+		user     => "${alfresco_db_user}",
+		password => "${alfresco_db_pass}",
+		host     => "${alfresco_db_host}",
+		grant    => ['ALL'],
+	}
+
+
+	class { '::mysql::bindings':
+		java_enable => 1,
+	}
+
+
+	exec { "retrieve-mysql-connector":
+		command => "wget ${urls::mysql_connector_url}",
+		cwd => "${download_path}",
+		path => "/usr/bin",
+		creates => "${download_path}/${urls::mysql_connector_file}",
+	}
+
+	exec { "unpack-mysql-connector":
+		command => "tar xzvf ${urls::mysql_connector_file}",
+		cwd => $download_path,
+		path => "/bin",
+		require => Exec["retrieve-mysql-connector"],
+		creates => "${download_path}/${urls::mysql_connector_name}",
+	}
+
+	exec { "copy-mysql-connector":
+		command => "cp ${download_path}/${urls::mysql_connector_name}/${urls::mysql_connector_name}-bin.jar  ${tomcat_home}/shared/lib/",
+		path => "/bin:/usr/bin",
+		require => [
+			Exec["unpack-mysql-connector"],
+			File["${tomcat_home}/shared/lib"],
+		],
+		creates => "${tomcat_home}/shared/lib/${urls::mysql_connector_name}-bin.jar",
+	}
+
+}
