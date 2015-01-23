f8b9742b89dd5ca02e334770c75700b3917569fe
Martin Cosgrave <martin@ciderpunk.com>
2015-01-23 12:50:01 +0100

    try to fix parse model error on slow vagrant

--- manifests/install/solr.pp
+++ manifests/install/solr.pp
@@ -99,7 +99,10 @@ class alfresco::install::solr inherits alfresco {
         owner => 'tomcat7',
       }
 
-
+      file { "${tomcat_home}/webapps/alfresco/WEB-INF/lib/solr-solrj-1.4.1.jar":
+        ensure => absent,
+        require => Exec['unpack-alfresco-war'],
+      }
 
     }
 
