63d69f2cfc66194df3ad4e9bad8e709925864259
Martin Cosgrave <martin@ciderpunk.com>
2015-01-23 17:14:40 +0100

    fix root password

--- manifests/install/mysql.pp
+++ manifests/install/mysql.pp
@@ -1,31 +1,32 @@
 class alfresco::install::mysql inherits alfresco {
 
 
-  if($alfresco_db_host != 'localhost') {
+  if $db_host == 'localhost'  {
 
 	  class { '::mysql::server':
-		  #root_password    => $db_root_password,
+		  root_password    => $db_root_password,
+      remove_default_accounts=> true,
+      service_enabled => true,
 	  }
 
     # for some reason setting it above does not always(?) work
     exec { "Set MySQL server root password":
-      subscribe => [ Class["::mysql::server"] ],
-      refreshonly => true,
+      subscribe => [ Class["mysql::server"] ],
+      #refreshonly => true,
       unless => "mysqladmin -uroot -p$db_root_password status",
       path => "/bin:/usr/bin",
       command => "mysqladmin -uroot password $db_root_password",
     }
 
+	  mysql::db { "$alfresco_db_name":
+		  user     => "${alfresco_db_user}",
+		  password => "${alfresco_db_pass}",
+		  host     => "${alfresco_db_host}",
+		  grant    => ['ALL'],
+	  }
+
   }
 
-
-
-	mysql::db { "$alfresco_db_name":
-		user     => "${alfresco_db_user}",
-		password => "${alfresco_db_pass}",
-		host     => "${alfresco_db_host}",
-		grant    => ['ALL'],
-	}
 
 
 	class { '::mysql::bindings':
