c880cdd944b4a5de7dbe960b1a6dc9683e64324e
digcat <info@digcat.com>
2015-01-28 10:49:57 +0000

    modify script to work, cleans out temp/work and webapps app folder

--- files/clean_tomcat.sh
+++ files/clean_tomcat.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-# ---------------------------------
-# Script to clean Tomcat temp files
-# ---------------------------------
+# ---------------------------------------------------------------
+# Script to clean Tomcat temp files and tomcat apps folders
+# ---------------------------------------------------------------
 echo "Cleaning temporary Alfresco files from Tomcat..."
-rm -rf tomcat/temp/Alfresco tomcat/work/Catalina/localhost/alfresco
-rm -rf tomcat/work/Catalina/localhost/share
-rm -rf tomcat/work/Catalina/localhost/awe
-rm -rf tomcat/work/Catalina/localhost/wcmqs
+sudo rm -rf ../tomcat/temp/Alfresco tomcat/work/Catalina/localhost/alfresco
+sudo rm -rf ../tomcat/work/Catalina/localhost/share
+sudo rm -rf ../tomcat/work/Catalina/localhost/awe
+sudo rm -rf ../tomcat/work/Catalina/localhost/wcmqs
+sudo rm -rf ../tomcat/webapps/solr4
+sudo rm -rf ../tomcat/webapps/share
+sudo rm -rf ../tomcat/webapps/alfresco
