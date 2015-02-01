f24f46cf0602a0ef2e4d7f432751b71070d36b03
Martin Cosgrave <martin@ciderpunk.com>
2015-02-01 16:49:47 +0100

    add puppetlabs-concat

--- install/modules-for-vagrant.sh
+++ install/modules-for-vagrant.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+MODULES="puppetlabs-mysql puppetlabs-stdlib stankevich-python puppetlabs-apache puppetlabs-apt\
+  puppetlabs-concat"
 
 mkdir -p modules
-for mod in puppetlabs-mysql puppetlabs-stdlib stankevich-python puppetlabs-apache
+for mod in $MODULES
 do
 	puppet module install --force $mod --target-dir=modules
 done
