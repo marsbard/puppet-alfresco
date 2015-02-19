9bdc04cdd7f2af734e324f2841d52e7c20ddb12f
Martin Cosgrave <martin@ciderpunk.com>
2015-02-19 13:01:00 +0100

    added epel module when centos or redhat is detected

--- install/modules-for-vagrant.sh
+++ install/modules-for-vagrant.sh
@@ -3,6 +3,13 @@
 MODULES="puppetlabs-mysql puppetlabs-stdlib stankevich-python puppetlabs-apache puppetlabs-apt\
   puppetlabs-concat"
 
+OS=`head -n1 /etc/issue | cut -f1 -d\ `
+
+if [ "$OS" == "RedHat" -o "$OS" == "CentOS" ]
+then
+  MODULES="$MODULES stahnma-epel"
+fi
+
 mkdir -p modules
 for mod in $MODULES
 do
