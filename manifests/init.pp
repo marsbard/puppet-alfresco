32046f340905e9694d45899a12a219b158243ccc
Martin Cosgrave <martin@ciderpunk.com>
2015-03-21 06:18:33 +0100

    added notice about which alfresco_version

--- manifests/init.pp
+++ manifests/init.pp
@@ -101,6 +101,7 @@ class alfresco (
 
 	$admin_pass_hash = calc_ntlm_hash($initial_admin_pass)
 
+  notice("alfresco_version = ${alfresco_version}")
 
 	# add JAVA_OPTS with memory settings - TODO this won't work for CentOS
 	$java_opts = "-Xmx${mem_xmx} -Xms${mem_xmx} -XX:MaxPermSize=${mem_xxmaxpermsize} -server"
