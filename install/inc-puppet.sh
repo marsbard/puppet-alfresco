e4062a6e1ea1437f863d213e38559087c5aa48a1
Martin Cosgrave <martin@ciderpunk.com>
2015-06-03 22:33:36 +0200

    do not use issue file to detect OS variant fixes #80

--- install/inc-puppet.sh
+++ install/inc-puppet.sh
@@ -1,15 +1,14 @@
 
 function install_puppet {
-  OS=`head -n1 /etc/issue | cut -f1 -d\ `
 	echo Installing puppet
-	if [ "$OS" == "CentOS" -o "$OS" == "RedHat" ]
+	if [ -f /etc/redhat-release ]
 	then
 		EL_MAJ_VER=`head -n1 /etc/issue | cut -f3 -d\ | cut -f1 -d.`
 		rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-${EL_MAJ_VER}.noarch.rpm
 		yum install -y puppet
 	fi
 
-	if [ "$OS" == "Debian" -o "$OS" == "Ubuntu" ]
+	if [ -f /etc/debian_version ]
 	then
 		apt-get update
 		apt-get install apt-utils -y
