264a728abc2f79445e153c73ef6d284157538ca1
Martin Cosgrave <martin@bettercode.com>
2015-08-02 09:30:02 +0200

    try to break dep cycle by moving jdk invocation to install.pp

--- manifests/install/jdk.pp
+++ manifests/install/jdk.pp
@@ -7,18 +7,18 @@ class alfresco::install::jdk inherits alfresco {
     'Debian': {
 
       $java_release = '8u45-b14-1'
-      $java_base_url = 'http://mirrors.kernel.org/ubuntu/pool/universe/o/openjdk-8/'
+      $java_base_url = 'http://mirrors.kernel.org/ubuntu/pool/universe/o/openjdk-8'
 
       if $java_version == 8 {
         # packages not yet in trusty
         # get the vivid ones
 
         safe-download { 'openjdk-8-jre-headless':
-          url => "${java_base_url}/openjdk-8-jre-headless_${java_release}.deb",
+          url => "${java_base_url}/openjdk-8-jre-headless_${java_release}_amd64.deb",
           filename => "openjdk-8-jre-headless_${java_release}.deb",
           download_path => $download_path,
         } -> exec {'gdebi-jre-headless':
-          command => '/usr/bin/gdebi -n ${download_path}/openjdk-8-jre-headless_${java_release}.deb',
+          command => '/usr/bin/gdebi -n ${download_path}/openjdk-8-jre-headless_${java_release}_amd64.deb',
         }
 
         safe-download { 'openjdk-8-jre':
@@ -26,7 +26,7 @@ class alfresco::install::jdk inherits alfresco {
           filename => "openjdk-8-jre_${java_release}.deb",
           download_path => $download_path,
         } -> exec {'gdebi-jre':
-          command => '/usr/bin/gdebi -n ${download_path}/openjdk-8-jre_${java_release}.deb',
+          command => '/usr/bin/gdebi -n ${download_path}/openjdk-8-jre_${java_release}_amd64.deb',
         }
 
         safe-download { 'openjdk-8-jdk':
