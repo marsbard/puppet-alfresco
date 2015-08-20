87220710192c161ee0ac088823ed483c224950e0
Martin Cosgrave <martin@ciderpunk.com>
2015-08-20 20:17:32 +0200

    fix deps for elastic

--- manifests/install/graylog.pp
+++ manifests/install/graylog.pp
@@ -1,6 +1,19 @@
 class alfresco::install::graylog inherits alfresco::install {
-  
-  class { 'mongodb': } ->
+
+  file { "/etc/apt/apt.conf.d/99auth":       
+    owner     => root,
+    group     => root,
+    content   => "APT::Get::AllowUnauthenticated yes;",
+    mode      => 644;
+  } ->
+  apt::source { 'elastic':
+    location => 'http://packages.elasticsearch.org/elasticsearch/1.4/debian',
+    release => 'stable',
+    repos => 'main',
+    include => { 'deb' => true },
+  } ->
   class { 'elasticsearch': } ->
+  class { 'mongodb': } ->
   class { 'graylog2': }
+
 }
