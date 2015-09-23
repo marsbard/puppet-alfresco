8e8c9d5a092d1b4acac7658458573ce33848e88d
darenf <daren@digcat.com>
2015-09-23 16:22:52 +0100

    Fix tabs for spaces

--- manifests/nightly.pp
+++ manifests/nightly.pp
@@ -3,11 +3,11 @@ class alfresco::nightly inherits alfresco{
   case $alfresco_version {
     'NIGHTLY': {
 
-				safe-download { 'nightly':
-					url => "${urls::nightly}",
-					filename => "${urls::nightly_filename}",
-					download_path => $download_path,
-				}
+        safe-download { 'nightly':
+          url => "${urls::nightly}",
+          filename => "${urls::nightly_filename}",
+          download_path => $download_path,
+        }
 
         exec { 'unpack-nightly':
           user => 'tomcat',
@@ -44,14 +44,14 @@ class alfresco::nightly inherits alfresco{
         exec { "${tomcat_home}/webapps/alfresco.war":
           user => 'tomcat',
           command => "touch /tmp/fake.get.alfresco.war",
-					path => '/bin:/usr/bin',
+          path => '/bin:/usr/bin',
           creates => "/tmp/fake.get.alfresco.war",
         }
 
         exec { "${tomcat_home}/webapps/share.war":
           user => 'tomcat',
           command => "touch /tmp/fake.get.share.war",
-					path => '/bin:/usr/bin',
+          path => '/bin:/usr/bin',
           creates => "/tmp/fake.get.share.war",
         }
     }
