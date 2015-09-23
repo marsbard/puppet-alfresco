63aa121c503d148a6ebc45407a344d01e5149a3b
Martin Cosgrave <martin@bettercode.com>
2015-09-23 19:59:55 +0200

    remove dep on unpacking wars from service.pp and hope that ordering helps us

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,17 @@
 class alfresco::service inherits alfresco {
 
 
-	service { 'alfresco-start':
-		name => 'tomcat',
-		ensure => running,
-		enable => true,
-		subscribe => [
-			File["${tomcat_home}/shared/classes/alfresco-global.properties"],
-			#Exec["${tomcat_home}/webapps/alfresco.war"],
-			#Exec["${tomcat_home}/webapps/share.war"],
-      Exec["unpack-alfresco-war"],
-      Exec["unpack-share-war"],
-		],
-	}
+  service { 'alfresco-start':
+    name => 'tomcat',
+    ensure => running,
+    enable => true,
+    subscribe => [
+      File["${tomcat_home}/shared/classes/alfresco-global.properties"],
+      #Exec["${tomcat_home}/webapps/alfresco.war"],
+      #Exec["${tomcat_home}/webapps/share.war"],
+      #Exec["unpack-alfresco-war"],
+      #Exec["unpack-share-war"],
+    ],
+  }
 
 }
