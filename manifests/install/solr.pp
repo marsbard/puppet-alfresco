f6a5a95f98f980313cd0b91be88cf66260ebacda
Martin Cosgrave <martin@bettercode.com>
2015-09-23 20:13:15 +0200

    partial rollback of nightly stuff

--- manifests/install/solr.pp
+++ manifests/install/solr.pp
@@ -3,52 +3,52 @@ class alfresco::install::solr inherits alfresco {
   case ($alfresco_version) {
     '4.2.f': {
 
-			safe-download { 'solr':
-				url => "${urls::solr_dl}",
-				filename => 'solr.zip',
-				download_path => $download_path,
-			}
+      safe-download { 'solr':
+        url => "${urls::solr_dl}",
+        filename => 'solr.zip',
+        download_path => $download_path,
+      }
 
-			exec { "unpack-solr":
+      exec { "unpack-solr":
         user => 'tomcat',
-				command => "unzip ${download_path}/solr.zip -d solr/",
-				cwd => $alfresco_base_dir,
-				path => '/usr/bin',
-				creates => "${alfresco_base_dir}/solr/solr.xml",
-				require => [
-					Safe-download["solr"],
-				],
-			}
+        command => "unzip ${download_path}/solr.zip -d solr/",
+        cwd => $alfresco_base_dir,
+        path => '/usr/bin',
+        creates => "${alfresco_base_dir}/solr/solr.xml",
+        require => [
+          Safe-download["solr"],
+        ],
+      }
 
-			file { "${alfresco_base_dir}/solr/alf_data":
-				ensure => absent,
-				force => true,
-				require => Exec["unpack-alfresco-ce"],
-				before => Service["alfresco-start"],
+      file { "${alfresco_base_dir}/solr/alf_data":
+        ensure => absent,
+        force => true,
+        require => Exec["unpack-alfresco-ce"],
+        before => Service["alfresco-start"],
         owner => 'tomcat',
-			}
+      }
 
 
-			file { "${alfresco_base_dir}/solr/workspace-SpacesStore/conf/solrcore.properties":
-				require => Exec['unpack-solr'],
-				content => template('alfresco/solrcore-workspace.properties.erb'),
-				ensure => present,
+      file { "${alfresco_base_dir}/solr/workspace-SpacesStore/conf/solrcore.properties":
+        require => Exec['unpack-solr'],
+        content => template('alfresco/solrcore-workspace.properties.erb'),
+        ensure => present,
         owner => 'tomcat',
-			}
+      }
 
 
-			file { "${alfresco_base_dir}/solr/archive-SpacesStore/conf/solrcore.properties":
-				require => Exec['unpack-solr'],
-				content => template('alfresco/solrcore-archive.properties.erb'),
-				ensure => present,
+      file { "${alfresco_base_dir}/solr/archive-SpacesStore/conf/solrcore.properties":
+        require => Exec['unpack-solr'],
+        content => template('alfresco/solrcore-archive.properties.erb'),
+        ensure => present,
         owner => 'tomcat',
-			}
+      }
 
-			file { "${tomcat_home}/conf/Catalina/localhost/solr.xml":
+      file { "${tomcat_home}/conf/Catalina/localhost/solr.xml":
         content => template('alfresco/solr.xml.erb'),
-				ensure => present,
+        ensure => present,
         owner => 'tomcat',
-			}
+      }
 
 
 
@@ -74,11 +74,11 @@ class alfresco::install::solr inherits alfresco {
 
     '5.0.c', '5.0.x', 'NIGHTLY': {
 
-			safe-download { 'solr-war':
-				url => "${urls::solr_war_dl}",
-				filename => "solr4.war",
-				download_path => "${tomcat_home}/webapps",
-			}
+      safe-download { 'solr-war':
+        url => "${urls::solr_war_dl}",
+        filename => "solr4.war",
+        download_path => "${tomcat_home}/webapps",
+      }
 
       file { "${alfresco_base_dir}/solr4":
         ensure => directory,
@@ -91,18 +91,18 @@ class alfresco::install::solr inherits alfresco {
         owner => 'tomcat',
       }
 
-			safe-download { 'solr-cfg':
-				url => "${urls::solr_cfg_dl}",
-				filename => 'solrconfig.zip',
-				download_path => $download_path,
-			}
+      safe-download { 'solr-cfg':
+        url => "${urls::solr_cfg_dl}",
+        filename => 'solrconfig.zip',
+        download_path => $download_path,
+      }
 
       exec { "unpack-solr-cfg":
         user => 'tomcat',
         command => "unzip -o ${download_path}/solrconfig.zip",
-				cwd => "${alfresco_base_dir}/solr4",
-				path => '/usr/bin',
-				creates => "${alfresco_base_dir}/solr4/context.xml",
+        cwd => "${alfresco_base_dir}/solr4",
+        path => '/usr/bin',
+        creates => "${alfresco_base_dir}/solr4/context.xml",
         require => Safe-download['solr-cfg'],
         notify => Service['alfresco-start'],
       }
