8ac8c5b28b8cb45d9bcb01520d436bd4d529085b
darenf <daren@digcat.com>
2015-11-17 11:34:28 +0000

    fix up more tabs

--- manifests/addons/aaar.pp
+++ manifests/addons/aaar.pp
@@ -1,6 +1,6 @@
 class alfresco::addons::aaar inherits alfresco::addons {
 
-	$aaarbase = "https://github.com/fcorti/alfresco-audit-analysis-reporting/releases/download/v4.0"
+  $aaarbase = "https://github.com/fcorti/alfresco-audit-analysis-reporting/releases/download/v4.0"
 
 case($alfresco_version){
       '4.2.f': {
@@ -8,32 +8,32 @@ case($alfresco_version){
           $aaarsharefile = "AAAR-Alfresco-CE-v4.2-Share-v1.2.amp"
       }
   		'5.0.x': {
-				  $aaarrepofile = "AAAR-Alfresco-CE-v5.0.d-Repository-v1.2.amp"
-					$aaarsharefile = "AAAR-Alfresco-CE-v5.0.d-Share-v1.2.amp"
+          $aaarrepofile = "AAAR-Alfresco-CE-v5.0.d-Repository-v1.2.amp"
+          $aaarsharefile = "AAAR-Alfresco-CE-v5.0.d-Share-v1.2.amp"
       }
-			'5.1.x': {
+      '5.1.x': {
           $aaarrepofile = "AAAR-Alfresco-CE-v5.1-Repository-v1.2.amp"
-					$aaarsharefile = "AAAR-Alfresco-CE-v5.1-Share-v1.2.amp"
-			}
-			'NIGHTLY': {
+          $aaarsharefile = "AAAR-Alfresco-CE-v5.1-Share-v1.2.amp"
+      }
+      'NIGHTLY': {
           $aaarrepofile = "AAAR-Alfresco-CE-v5.1-Repository-v1.2.amp"
-					$aaarsharefile = "AAAR-Alfresco-CE-v5.1-Share-v1.2.amp"
-			}
+          $aaarsharefile = "AAAR-Alfresco-CE-v5.1-Share-v1.2.amp"
+      }
 }
   $aaarshareurl = "${aaarbase}/${aaarsharefile}"
   $aaarrepourl = "${aaarbase}/${aaarrepofile}"
 
 
-	safe-download { 'aaar-repo':
-		url => $aaarrepourl,
-		filename => $aaarrepofile,
-		download_path => "${alfresco_base_dir}/amps",
-	}
+  safe-download { 'aaar-repo':
+    url => $aaarrepourl,
+    filename => $aaarrepofile,
+    download_path => "${alfresco_base_dir}/amps",
+  }
 
-	safe-download { 'aaar-share':
-		url => $aaarshareurl,
-		filename => $aaarsharefile,
-		download_path => "${alfresco_base_dir}/amps_share",
-	}
+  safe-download { 'aaar-share':
+    url => $aaarshareurl,
+    filename => $aaarsharefile,
+    download_path => "${alfresco_base_dir}/amps_share",
+  }
 
 }
