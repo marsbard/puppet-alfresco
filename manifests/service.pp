dd10cc3af6e40f740b4781f894f097627b1b2b97
Martin Cosgrave <martin@ciderpunk.com>
2016-05-07 13:54:36 +0200

    added systemd startup for centos

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,10 +7,9 @@ class alfresco::service inherits alfresco {
     enable => true,
     subscribe => [
       File["${tomcat_home}/shared/classes/alfresco-global.properties"],
-      #Exec["${tomcat_home}/webapps/alfresco.war"],
-      #Exec["${tomcat_home}/webapps/share.war"],
       Exec["unpack-alfresco-war"],
       Exec["unpack-share-war"],
+			File["/etc/systemd/system/tomcat.service"],
     ],
   }
 
