7bfd6c62711b53528f33cb7eda658b26d8f786cf
Martin Cosgrave <martin@ciderpunk.com>
2016-05-07 19:59:16 +0200

    parameterise img_coders so centos_42x can has thumbs

--- manifests/install.pp
+++ manifests/install.pp
@@ -360,6 +360,8 @@ class alfresco::install inherits alfresco {
 # trying to keep swftools config separate in case I can find a build with pdf2swf in
 ###################################################
 
+  $img_coders = "/usr/lib/x86_64-linux-gnu/ImageMagick-6.7.7/modules-Q16/coders"
+
   case $::osfamily {
     'RedHat': {
       $swfpkgs = [
@@ -371,6 +373,8 @@ class alfresco::install inherits alfresco {
         "gcc",
         "gcc-c++"
       ]
+
+      $img_coders = "/usr/lib64/ImageMagick-6.7.8/modules-Q16/coders"
 
       safe-download { 'swftools':
         url => $urls::swftools_src_url,
