dea468a9f31257602a388acd0b2212d2b76ab329
digcat <info@digcat.com>
2016-05-25 09:15:49 -0400

    Fix permissions on new db

--- manifests/install/postgresql.pp
+++ manifests/install/postgresql.pp
@@ -1,11 +1,21 @@
 class alfresco::install::postgresql inherits alfresco {
     if $db_host == 'localhost' and $db_type == 'postgresql'  {
-    
+        
     class { 'postgresql::globals':
+       encoding => 'UTF-8',
+       locale   => 'en_US.UTF-8',
        manage_package_repo => true,
        version             => '9.3',
     }->
     class { '::postgresql::server': }
+       postgresql::server::role { 'marmot':
+          password_hash => postgresql_password("${alresco_db_user}", "${alfresco_db_pass}"),
+       }
+       postgresql::server::database_grant { "${alfresco_db_name}":
+          privilege => 'ALL',
+          db        => "${alfresco_db_name}",
+          role      => "${alfresco_db_user}",
+       }
        postgresql::server::db { "${alfresco_db_name}":
           user     => "${alfresco_db_user}",
           password => postgresql_password("${alfresco_db_name}","${alfresco_db_pass}"),
