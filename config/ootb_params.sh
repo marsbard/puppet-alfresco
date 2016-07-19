931054666d7db8e771f3a66424e0c32dfe7390cf
Martin Cosgrave <martin@bettercode.com>
2016-07-19 12:12:56 +0200

    added validation for db_type and moved it above other db params

--- config/ootb_params.sh
+++ config/ootb_params.sh
@@ -55,6 +55,12 @@ params[$IDX]="download_path"
 descr[$IDX]="Where to store downloaded files"
 default[$IDX]="/opt/downloads"
 
+IDX=$(( $IDX + 1 ))
+params[$IDX]="db_type"
+descr[$IDX]="Database type"
+default[$IDX]="mysql"
+choices[$IDX]="mysql|postgresql"
+
 
 IDX=$(( $IDX + 1 ))
 params[$IDX]="db_root_password"
@@ -90,11 +96,6 @@ IDX=$(( $IDX + 1 ))
 params[$IDX]="db_port"
 descr[$IDX]="Database port"
 default[$IDX]="3306"
-
-IDX=$(( $IDX + 1 ))
-params[$IDX]="db_type"
-descr[$IDX]="Database type"
-default[$IDX]="mysql"
 
 IDX=$(( $IDX + 1 ))
 params[$IDX]="mem_xmx"
